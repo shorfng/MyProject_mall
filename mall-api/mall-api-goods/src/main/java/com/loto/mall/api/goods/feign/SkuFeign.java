14980624d60b00e317acc68826e6b36d68c8e3c7
蓝田_Loto <shorfng@126.com>
2022-06-16 00:13:05 +0800

    feign接口编写

---
+++ mall-api/mall-api-goods/src/main/java/com/loto/mall/api/goods/feign/SkuFeign.java
@@ -0,0 +1,36 @@
+package com.loto.mall.api.goods.feign;
+
+import com.loto.mall.api.goods.model.Sku;
+import com.loto.mall.util.common.RespResult;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * Author：蓝田_Loto<p>
+ * Date：2022-06-16 0:01<p>
+ * PageName：SkuFeign.java<p>
+ * Function：
+ */
+
+@FeignClient(value = "mall-goods")    //服务名字
+@RequestMapping("/sku")
+public interface SkuFeign {
+    @ApiOperation(value = "根据商品推广分类id，查询指定分类下的产品列表")
+    @GetMapping(value = "/aditems/type/{typeId}")
+    List<Sku> typeItems(@PathVariable(value = "typeId") Integer typeId);
+
+    @ApiOperation(value = "删除缓存（根据商品推广分类id，查询指定分类下的产品列表）")
+    @DeleteMapping(value = "/aditems/type/{typeId}")
+    RespResult delTypeItems(@PathVariable(value = "typeId") Integer typeId);
+
+    @ApiOperation(value = "修改缓存（根据商品推广分类id，查询指定分类下的产品列表）")
+    @PutMapping(value = "/aditems/type/{typeId}")
+    List<Sku> updateTypeItems(@PathVariable(value = "typeId") Integer typeId);
+}
