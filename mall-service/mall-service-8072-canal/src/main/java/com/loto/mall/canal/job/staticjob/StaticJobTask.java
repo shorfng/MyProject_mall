5282d58403afbfb6758002937ccb9458d6846c99
蓝田_Loto <shorfng@126.com>
2022-06-28 11:36:46 +0800

    elaticjob - 静态定时任务

---
+++ mall-service/mall-service-8072-canal/src/main/java/com/loto/mall/canal/job/staticjob/StaticJobTask.java
@@ -0,0 +1,26 @@
+package com.loto.mall.canal.job.staticjob;
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import com.dangdang.elasticjob.lite.annotation.ElasticSimpleJob;
+import org.springframework.stereotype.Component;
+
+/**
+ * Author：蓝田_Loto<p>
+ * Date：2022-06-27 19:17<p>
+ * PageName：StaticJobTask.java<p>
+ * Function：
+ */
+
+// 参数1：jobName（指定 Zookeeper 中的命名空间）
+// 参数2：shardingTotalCount（分片）
+// 参数3：cron（执行周期）
+@ElasticSimpleJob(jobName = "${elaticjob.zookeeper.namespace}", shardingTotalCount = 1, cron = "0/5 * * * * ? *")
+@Component
+public class StaticJobTask implements SimpleJob {
+    // 执行的作业
+    @Override
+    public void execute(ShardingContext shardingContext) {
+        System.out.println("StaticJobTask-----------execute");
+    }
+}
