63c2f3b5662387c7d55eb820cbd068e72aba1199
蓝田_Loto <shorfng@126.com>
2022-07-04 17:34:26 +0800

    解决文件重复问题

--- mall-gateway/src/main/java/com/loto/mall/gateway/lock/RedissonLock.java
+++ mall-gateway/src/main/java/com/loto/mall/gateway/lock/RedissonLock.java
@@ -3,8 +3,8 @@ package com.loto.mall.gateway.lock;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Author：蓝田_Loto<p>
@@ -13,17 +13,16 @@ import org.springframework.context.annotation.Bean;
  * Function：
  */
 
-//@Configuration
+@Configuration
 public class RedissonLock {
-    @Value("${spring.redis.host}")
-    private String redisHost;
+    //@Value("${spring.redis.host}")
+    //private String redisHost;
 
-    @Value("${spring.redis.password}")
-    private String redisPassword;
+    //@Value("${spring.redis.password}")
+    //private String redisPassword;
 
-    /***
-     * 创建RedissonClient对象
-     *      创建锁、解锁
+    /**
+     * 创建 RedissonClient 对象、创建锁、解锁
      * @return
      */
     @Bean
@@ -32,18 +31,18 @@ public class RedissonLock {
         Config config = new Config();
 
         // 集群实现
-        //config.useClusterServers()
-        //        .setScanInterval(2000)
-        //        .addNodeAddress(
-        //                "redis://192.168.126.200:7001",
-        //                "redis://192.168.126.200:7002",
-        //                "redis://192.168.126.200:7003",
-        //                "redis://192.168.126.200:7004",
-        //                "redis://192.168.126.200:7005",
-        //                "redis://192.168.126.200:7006");
+        config.useClusterServers()
+                .setScanInterval(2000)
+                .addNodeAddress(
+                        "redis://192.168.126.200:7001",
+                        "redis://192.168.126.200:7002",
+                        "redis://192.168.126.200:7003",
+                        "redis://192.168.126.200:7004",
+                        "redis://192.168.126.200:7005",
+                        "redis://192.168.126.200:7006");
 
         // 单机实现
-        config.useSingleServer().setAddress("redis://" + redisHost + ":6379").setPassword(redisPassword);
+        //config.useSingleServer().setAddress("redis://" + redisHost + ":6379").setPassword(redisPassword);
 
         // 创建 RedissonClient
         return Redisson.create(config);
